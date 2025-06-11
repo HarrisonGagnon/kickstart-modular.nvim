cd1f567f963cd8de690b62bbb5be559be09eafcb
Harrison <harrisongagnon@gmail.com>
2025-06-11 02:33:33 -0400

    Added initial java config.

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,10 +1,15 @@
 return {
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'catppuccin-mocha'
-    end,
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  opts = {
+    no_italic = true, -- Force no italic
   },
+
+  init = function()
+    -- Load the colorscheme here.
+    vim.cmd.colorscheme 'catppuccin-mocha'
+  end,
 }
+
+-- vim: ts=2 sts=2 sw=2 et
